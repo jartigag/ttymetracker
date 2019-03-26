f24829d00abd4d6e9ca5b6310fe9ad65f6550b8b
jartigag <jartigag@protonmail.com>
2019-03-26 16:15:34 +0100

    wip4: ttymetracker_anuko

--- modules/ttymetracker_anuko.py
+++ modules/ttymetracker_anuko.py
@@ -13,15 +13,14 @@
 __author__ = "@jartigag"
 __version__ = "0.4"
 
-#TODO1: ttytracker_aliases.cfg: a config json that matches shortcuts with anuko projects and clients.
-#                               for example: 
-#                               for example: [{"shorcut": "soporteSamsung", "project": "I_207_SAMSUNG_19_Soporte_19", "client": "Samsung"}, {"shorcut": "devSamsung", ...}]
-#                                            └── #soporteSamsung = I_207_SAMSUNG_19_Soporte_19 @ Samsung | #devSamsung = I_208_SAMSUNG_19_Desarrollo_19 @ Samsung
+# ttytracker_aliases.cfg: a config json that matches shortcuts with anuko projects and clients.
+#                         for example:
+#                         for example: [{"shorcut": "soporteSamsung", "project": "I_207_SAMSUNG_19_Soporte_19", "client": "Samsung"}, {"shorcut": "devSamsung", ...}]
+#                                       └── #soporteSamsung = I_207_SAMSUNG_19_Soporte_19 @ Samsung | #devSamsung = I_208_SAMSUNG_19_Desarrollo_19 @ Samsung
 #
-#TODO2: open a commit.tmp file in vim before posting data to anuko
 
 from bs4 import BeautifulSoup
-import urllib.request
+import urllib.request, urllib.parse
 import ssl
 from ttymetracker_credentials import *
 import json
@@ -40,31 +39,38 @@ def commit_today(logbooksDir, aliasesFile, round_time_to_quarter_hour=False):
             c.write('\n##~ Elimina esta línea para confirmar la publicación de este registro ~##\n\n'.format(today_short))
             c.write("## DESDE - HASTA   | NOTA\n")
             lines = f.readlines()
-            time = '09:00:00'
+            time = '09:00'
             for line in lines:
                 i = lines.index(line)
                 if i==len(lines)-1: break
                 if lines[i+1]=='---\n': # if this line it's a timestamp:
                     if lines[i+2][0]=='>':    # if there's a note on this timestamp:
                         previous_time = time #TODO: optional argument to round time to quarter of an hour
-                        time = ''.join( line.split(' ')[4] ) # line.split(' ')[4] takes something like '09:17:43'
+                        time = ''.join( line.split(' ')[4][:-3] ) # line.split(' ')[4] takes something like '09:17'
                         c.write("{} - {}: {}".format(previous_time, time, lines[i+2][2:]))
         os.system("vim commit.tmp")
     except IOError as e:
         print("error: {}".format(e))
 
 def push_note(start, end, project, client, task, note):
-    r = urllib.request.Request(anuko_url, headers=anuko_cookie)
-    print('{} - {} | project: {}, client: {}, task: {}, note: {}'.format(start, end, project, client, task, note))
-    #WIP
-    html = urllib.request.urlopen(r).read()
-    soup = BeautifulSoup(html, 'html.parser')
-    clients = soup.findAll( lambda x: x.name=='option' and x.parent.attrs.get('name')=='client')
-    projects = soup.findAll( lambda x: x.name=='option' and x.parent.attrs.get('name')=='project')
-    #tasks = soup.findAll( lambda x: x.name=='option' and x.parent.attrs.get('name')=='task')
-    print('clients:',clients)
-    print('projects:',projects)
-    #print('tasks:',taks)
+    try:
+        r = urllib.request.Request(anuko_url, headers=anuko_cookie)
+        print('{} - {} {} | {} | project: {}, client: {}, task: {}'.format(start, end, datetime.now().strftime("%Y-%m-%d"), note, project, client, task)) #DEBUGGING
+        #WIP
+        html = urllib.request.urlopen(r).read()
+        soup = BeautifulSoup(html, 'html.parser')
+        clients = soup.findAll( lambda x: x.name=='option' and x.parent.attrs.get('name')=='client')
+        projects = soup.findAll( lambda x: x.name=='option' and x.parent.attrs.get('name')=='project')
+        #tasks = soup.findAll( lambda x: x.name=='option' and x.parent.attrs.get('name')=='task')
+        post_data = 'project=194&task=2&start={}&finish={}&date={}&note={}&btn_submit=Submit'.format(
+                    start.replace(':','%3A'), end.replace(':','%3A'), datetime.now().strftime("%Y-%m-%d"), urllib.parse.quote(note)
+                ).encode('utf8')
+        #print(post_data.decode('utf8'))
+        req = urllib.request.Request(anuko_url, headers=anuko_cookie, data=post_data)
+        ans = urllib.request.urlopen(r)
+        #print(ans.read())
+    except Exception as e:
+        print("error: {}".format(e))
 
 def push_today(aliasesFile):
     print("\033[1m[[ notas del registro de HOY {} para publicar en Anuko: ]]\033[0m\n".format(datetime.now().strftime("%Y-%m-%d")))
@@ -74,8 +80,8 @@ def push_today(aliasesFile):
             aliases = json.load( open(aliasesFile) )
         with open("commit.tmp") as f:
             lines = f.readlines()
-            project, client, task = '', '', ''
             for line in lines:
+                project, client, task = '', '', ''
                 line = line.rstrip() # remove '\n'
                 if line=='##~ Elimina esta línea para confirmar la publicación de este registro ~##':
                     print('acción cancelada (porque la línea \'##~ ~##\' no fue eliminada)')
@@ -89,36 +95,21 @@ def push_today(aliasesFile):
                         client = alias['client']
                         task = alias['task']
                 # from '16:00:00 - 17:00:00: [x] depurar Líneas Base',
-                start = line.split(' - ')[0]   # it takes 16:00:00
-                end = line.split(' - ')[1][:8] # it takes 17:00:00
-                note = line[21:]               # it takes '[x] depurar Líneas Base'
+                start = line.split(' - ')[0]   # it takes 16:00
+                end = line.split(' - ')[1][:5] # it takes 17:00
+                note = line[15:]               # it takes '[x] depurar Líneas Base'
                 push_note(start, end, project, client, task, note)
         os.system("rm commit.tmp")
     except IOError as e:
         print("error: {}".format(e))
 
-r = urllib.request.Request(anuko_url, headers=anuko_cookie)
-
-html = urllib.request.urlopen(r).read()
-soup = BeautifulSoup(html, 'html.parser')
-
-clients = soup.findAll( lambda x: x.name=='option' and x.parent.attrs.get('name')=='client')
-projects = soup.findAll( lambda x: x.name=='option' and x.parent.attrs.get('name')=='project')
-tasks = soup.findAll( lambda x: x.name=='option' and x.parent.attrs.get('name')=='task')
-
 '''
-POST /timetracker/time.php HTTP/1.1
-Host: localhost:8889
-User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
-Accept-Language: en-US,en;q=0.5
-Accept-Encoding: gzip, deflate
-Referer: http://localhost:8889/timetracker/time.php
-Content-Type: application/x-www-form-urlencoded
-Content-Length: 122
-Connection: close
-Cookie: tt_PHPSESSID=o99vjfr7ussbthrllp01jsk2v0; tt_login=jartiga
-Upgrade-Insecure-Requests: 1
-
-client=1&project=6&task=1&start=9%3A00&finish=13%3A30&date=2019-03-19&note=nota&btn_submit=Submit&browser_today=2019-03-19
+$ curl -XPOST 'https://localhost/time.php' -H 'Cookie: tt_login=jartiga; PHPSESSID=o99vjfr7ussbthrllp01jsk2v0; authchallenge=ef8902d732fa23d0c3b8099998265571' --insecure -d '
+client=36&project=194&task=2&start=9%3A00&finish=13%3A30&date=2019-03-26&note=nota2&btn_submit=Submit'
 '''
+'''
+$ curl -XPOST 'https://localhost/time.php' -H 'Cookie: tt_login=jartiga; PHPSESSID=o99vjfr7ussbthrllp01jsk2v0; authchallenge=ef8902d732fa23d0c3b8099998265571' --insecure -d '
+client=49&project=203&task=1&start=9%3A00&finish=13%3A30&date=2019-03-26&note=nota&btn_submit=Submit'
+'''
+#FIXME: client=XX no está funcionando
+#FIXME: task=XX no está cogiendo las tareas esperadas
