d0d4165aa2b44833b5f9d070374743a6f9cf6a42
jartigag <33371871+jartigag@users.noreply.github.com>
2019-06-22 20:51:45 +0200

    update ttymetracker_credentials.py

--- ttymetracker_credentials.py
+++ ttymetracker_credentials.py
@@ -2,15 +2,16 @@ anuko_url=''
 anuko_cookie=''
 anuko_headers=''
 
-sharepoint_url=''
-sharepoint_check_url=''
-sharepoint_listTitle = ''
-sharepoint_itemType=''
+sharepoint_url=''         #example: https://myCompany.sharepoint.com/sites/theAdminSite
+sharepoint_check_url=''   #example: https://myCompany.sharepoint.com/sites/theAdminSite/Lists/Horas_XX/AllItems.aspx
+sharepoint_listTitle = '' #example: Horas_XX
+sharepoint_itemType=''    #example: XX.Data.Horas_x005f_XXListItem
 sharepoint_username=''
 sharepoint_password=''
 sharepoint_userid=0
 
 git_remoteURL=''
-git_userBranch=''
 git_configName=''
 git_configEmail=''
+
+starting_hour='' #example: 09:00
